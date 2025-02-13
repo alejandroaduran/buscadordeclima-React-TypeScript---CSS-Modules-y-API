da4fcd363d4857be721d38b85c4494e2375354c3
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-02-12 23:55:19 -0300

    css forms

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,42 @@
+import { ChangeEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
+import type { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>(
+        {
+            city: "",
+            country: ""
+        }
+    )
+
+    const [alert, setAlert] = useState("");
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (Object.values(search).includes("")) {
+            setAlert("Todos los campos son obligatorios");
+            return;
+        }
+    }
+
     return (
-        <form action="" className={styles.form}>
+
+        <form
+            onSubmit={handleSubmit}
+            action="" className={styles.form}
+        >
+            {alert && <Alert>{alert}</Alert>}
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad: </label>
                 <input
@@ -11,11 +44,18 @@ export default function Form() {
                     type="text"
                     name="city"
                     placeholder="Ej. Buenos Aires"
+                    value={search.city}
+                    onChange={handleChange}
                 />
             </div>
             <div className={styles.field}>
-                <label htmlFor="pais">País: </label>
-                <select name="" id="">
+                <label htmlFor="country">País: </label>
+                <select
+                    name="country"
+                    id="country"
+                    value={search.country}
+                    onChange={handleChange}
+                >
                     <option value="">-- Seleccione un País --</option>
                     {countries.map((country) => (
                         <option key={country.code} value={country.code}>
@@ -24,7 +64,7 @@ export default function Form() {
                 </select>
             </div>
 
-            <input type="submit" value="Consultar clima" />
+            <input className={styles.submit} type="submit" value="Consultar clima" />
         </form>
     )
 }
